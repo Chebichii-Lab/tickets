68c9701030c6f0d6b4c6b15d60ac5209f0c4606c
Chebichii-Lab <chebichiiserem@gmail.com>
2024-06-03 21:19:34 +0300

    add url paths to the event functions

---
+++ events/urls.py
@@ -0,0 +1,10 @@
+# TicketPlatform/Events/urls.py
+from django.urls import path
+from .views import event_list, event_detail, add_event, purchase_ticket
+
+urlpatterns = [
+    path('', event_list, name='event_list'),
+    path('<int:event_id>/', event_detail, name='event_detail'),
+    path('add/', add_event, name='add_event'),
+    path('<int:event_id>/purchase/<str:ticket_type>/', purchase_ticket, name='purchase_ticket'),
+]
