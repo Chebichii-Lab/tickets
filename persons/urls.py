96b875df6967dc5d33015e35d3fda37fd2865fca
Chebichii-Lab <chebichiiserem@gmail.com>
2024-06-04 21:52:16 +0300

    add the url path for the logout view function

--- persons/urls.py
+++ persons/urls.py
@@ -1,13 +1,13 @@
 # TicketPlatform/Persons/urls.py
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import top_up_wallet, wallet_balance, index, sign_up, log_in
+from .views import top_up_wallet, wallet_balance, index, sign_up, log_in, log_out
 
 urlpatterns = [
     path('', index, name='index'),
     path('sign-up/', sign_up, name='sign_up'),
     path('log-in/', log_in, name='log_in'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', log_out, name='logout'),
     path('top-up/', top_up_wallet, name='top_up_wallet'),
     path('balance/', wallet_balance, name='wallet_balance'),
 ]
