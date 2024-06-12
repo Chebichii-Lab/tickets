bcab0515f8817fa99f91db2364788f0f8e10571f
Chebichii-Lab <chebichiiserem@gmail.com>
2024-06-12 08:51:00 +0300

    add location to the form fields

--- events/forms.py
+++ events/forms.py
@@ -5,14 +5,14 @@ from .models import Event, Ticket
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'description', 'date', 'price_vip', 'price_vvip', 'price_group', 'tickets_vip', 'tickets_vvip', 'tickets_group', 'image']
+        fields = ['name', 'description', 'date', 'price_vip', 'price_vvip', 'price_group', 'tickets_vip', 'tickets_vvip', 'tickets_group', 'image', 'location']
 
      # Optionally add custom widget attributes
     def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
-            
+
 class PurchaseTicketForm(forms.Form):
     TICKET_CHOICES = [
         ('vip', 'VIP'),
