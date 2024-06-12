fd310134062753d55a67409530f7388a457393e8
Chebichii-Lab <chebichiiserem@gmail.com>
2024-06-12 08:17:40 +0300

    add url patterns settings to accomodate media uploads

--- persons/urls.py
+++ persons/urls.py
@@ -1,6 +1,8 @@
 # TicketPlatform/Persons/urls.py
 from django.urls import path
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from ticket import settings
 from .views import top_up_wallet, wallet_balance, index, sign_up, log_in, log_out
 
 urlpatterns = [
@@ -11,3 +13,6 @@ urlpatterns = [
     path('top-up/', top_up_wallet, name='top_up_wallet'),
     path('balance/', wallet_balance, name='wallet_balance'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
