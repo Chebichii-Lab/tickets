93912cc396ceec81bf30fa8edf6003727d7f9edb
Chebichii-Lab <chebichiiserem@gmail.com>
2024-06-12 08:13:19 +0300

    add url pattern settings to accomodate images

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,7 @@
 # TicketPlatform/Events/urls.py
 from django.urls import path
+from django.conf.urls.static import static
+from ticket import settings
 from .views import event_list, event_detail, add_event, purchase_ticket
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path('add/', add_event, name='add_event'),
     path('<int:event_id>/purchase/<str:ticket_type>/', purchase_ticket, name='purchase_ticket'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
